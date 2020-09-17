3e7e8fcff023eb4d07c0c76de10c269bcfeeddd2
David Rodriguez <davisito544@gmail.com>
2020-09-17 11:56:11 -0500

    resultado del taller de local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,53 @@
-const btnDark = document.getElementById('dark')
+//funcion para mostar la paleta de temas
+const showDivThemes = () => {
+    const divTheme2 = document.querySelectorAll('.contTheme');
+    for (let i = 0; i < divTheme2.length; i++) {
+        divTheme2[i].addEventListener('mouseenter', () => {
+            divTheme.classList.add('show');
+            textTheme.classList.add('hide');
+            console.log(divTheme.classList);
+        });
+    }
+    for (let i = 0; i < divTheme2.length; i++) {
+        divTheme2[i].addEventListener('mouseleave', () => {
+            divTheme.classList.remove('show');
+            textTheme.classList.remove('hide');
+            console.log(divTheme.classList);
+        });
+    }
 
-btnDark.addEventListener('click', ()=>{
-    const container = document.getElementById('container').classList.add('dark')
-})
-
-const usuario = document.getElementById('usuario')
-
-usuario.addEventListener('click', ()=>{
-    const nombreUsuario = prompt('Escribe tu nombre')
-    localStorage.setItem("usuarioStorage", nombreUsuario)
-})
-
-
-const traerUsuarioStorage = ()=>{
-    const usuarioGuardadoEnStorage = localStorage.getItem('usuarioStorage')
-    document.getElementById('mostrarUsuario').innerText = usuarioGuardadoEnStorage
-    console.log(usuarioGuardadoEnStorage, "Este es el usuario")
 }
+// funcion para obtener el nombre del tema y realizar segun el caso el cambio de la clase
+const themeContainer = document.getElementById('container');
+let getTheme = (theme) => {
+    switch (theme) {
+        case 'dark':
+            themeContainer.className = "container dark";
+            setThemeStorage(themeContainer.classList[1]);
+            break;
+        case 'light':
+            themeContainer.className = "container light";
+            setThemeStorage(themeContainer.classList[1]);
+            break;
+        case 'pink':
+            themeContainer.className = "container pink";
+            setThemeStorage(themeContainer.classList[1]);
+            break;
+        case 'premium':
+            themeContainer.className = "container premium";
+            setThemeStorage(themeContainer.classList[1]);
+            break;
 
-traerUsuarioStorage()
+    }
+}
+//recibe el valor del tema para guardarlo en el localstorage
+let setThemeStorage = (themeSaved) => {
+    localStorage.setItem("theme", themeSaved);
+}
+//al cargar la pagina captura el valor del tema del localstorage y se muestra en la pagina
+let getThemeStorage = () => {
+    const savedTheme = localStorage.getItem('theme');
+    getTheme(savedTheme);
+}
+showDivThemes();
+getThemeStorage();
